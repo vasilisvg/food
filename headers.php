cfce872b3fc2b5ff72c023ea5160516ef2c1691e
Vasilis van Gemert <github@vasilis.nl>
2016-07-04 10:18:05 +0200

    X-Content-Type-Options

--- headers.php
+++ headers.php
@@ -2,4 +2,5 @@
 header('Strict-Transport-Security: max-age=15768000');
 header('X-Xss-Protection: 1; mode=block');
 header('X-Frame-Options: SAMEORIGIN');
+header('X-Content-Type-Options: nosniff');
 ?>
