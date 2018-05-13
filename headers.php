5b6398fae117d82021badf9ba56cbc37bfdd6f7e
Vasilis van Gemert <github@vasilis.nl>
2018-05-13 23:16:25 +0200

    'self'

--- headers.php
+++ headers.php
@@ -3,5 +3,5 @@ header('Strict-Transport-Security: max-age=15768000');
 header('X-Xss-Protection: 1; mode=block');
 header('X-Frame-Options: SAMEORIGIN');
 header('X-Content-Type-Options: nosniff');
-header("Content-Security-Policy: default-src 'self'; style-src 'unsafe-inline' https://fonts.typotheque.com/; font-src https://*.amazonaws.com/; script-src self 'unsafe-inline");
+header("Content-Security-Policy: default-src 'self'; style-src 'unsafe-inline' https://fonts.typotheque.com/; font-src https://*.amazonaws.com/; script-src 'self' 'unsafe-inline");
 ?>
