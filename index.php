28d093312ea2c07bf43fd8b676de328a6be80840
Vasilis van Gemert <github@vasilis.nl>
2018-05-13 23:14:31 +0200

    Made service worker accessible

--- index.php
+++ index.php
@@ -25,5 +25,19 @@ include 'css.css';
 <?php
 $home = 1;
 include 'recepten.php'; ?>
+<script>
+//This is the service worker with the Cache-first network
+
+//Add this below content to your HTML page, or add the js file to your page at the very top to register sercie worker
+if (navigator.serviceWorker.controller) {
+  console.log('[PWA Builder] active service worker found, no need to register')
+} else {
+
+//Register the ServiceWorker
+  navigator.serviceWorker.register('pwabuilder-sw.js').then(function(reg) {
+    console.log('Service worker has been registered for scope:'+ reg.scope);
+  });
+}
+</script>
 </body>
 </html>
