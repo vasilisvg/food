7844f01dcae4e7c666f8ef533c9fd959c6abb87d
Vasilis van Gemert <github@vasilis.nl>
2018-05-13 23:18:50 +0200

    path

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@ if (navigator.serviceWorker.controller) {
 } else {
 
 //Register the ServiceWorker
-  navigator.serviceWorker.register('pwabuilder-sw.js').then(function(reg) {
+  navigator.serviceWorker.register('../pwabuilder-sw.js').then(function(reg) {
     console.log('Service worker has been registered for scope:'+ reg.scope);
   });
 }
