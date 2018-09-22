5e94931f62bb2f8a7e798a6d71e97eabaed41c6d
Vasilis van Gemert <github@vasilis.nl>
2018-09-22 19:40:25 +0200

    Updated pasta di papa

--- pasta-di-papa/index.php
+++ pasta-di-papa/index.php
@@ -20,11 +20,11 @@ header h1 {
 				<li><span class="item-2">Oregano</span></li>
 				<li><span class="item-3">Thyme</span></li>
 				<li><span class="item-4">2 finely chopped onions</span></li>
-				<li><span class="item-5">quite some garlic</span></li>
+				<li><span class="item-5">Quite some garlic</span></li>
 				<li><span class="item-6">900 gr minced meat</span></li>
 				<li><span class="item-7">500 ml white wine</span></li>
 				<li><span class="item-8">1 can of tomatoes, blended</span></li>
-				<li><span class="item-9">Olives, finely chopped</span></li>
+				<li><span class="item-9">5 half dried tomatoes</span></li>
 				<li><span class="item-10">100 gr feta cheese, crumbled</span></li>
 				<li><span class="item-11">Pepper</span></li>
 				<li><span class="item-12">Salt</span></li>
@@ -34,11 +34,11 @@ header h1 {
 		</aside>
 		<div>
 			<h2>Prep</h2>
-			<p>Grind the <span class="item-2">oregano</span> and the <span class="item-3">thyme</span>, chop the <span class="item-4">onions</span>, finely chop the <span class="item-5">garlic</span>, blend the <span class="item-8">canned tomatoes</span> together with the <span class="item-9">olives</span>.</p>
+			<p>Grind the <span class="item-2">oregano</span> and the <span class="item-3">thyme</span>, chop the <span class="item-4">onions</span>, finely chop the <span class="item-5">garlic</span>, blend the <span class="item-8">canned tomatoes</span> together with the <span class="item-9">half dried tomatoes</span>. You can add some black olives to the blend as well if you want to.</p>
 
 			<h2>Cooking</h2>
 			<p>Put <span class="item-1">olive oil</span> in a pan over medium heat. Add the <span class="item-2">oregano</span> and <span class="item-3">thyme</span>. After a minute or so, add the <span class="item-4">onions</span> and the <span class="item-5">garlic</span>.</p>
-			<p>After five minutes or so put the pan on high heat and add the <span class="item-6">meat</span>. When the <span class="item-6">meat</span> is brown, add the <span class="item-7">wine</span>. When it’s cooking, add the <span class="item-8">tomatoes</span>, <span class="item-9">olives</span>, <span class="item-10">feta</span>, <span class="item-11">pepper</span> and <span class="item-12">salt</span>.</p>
+			<p>After five minutes or so put the pan on high heat and add the <span class="item-6">meat</span>. When the <span class="item-6">meat</span> is brown, add the <span class="item-7">wine</span>. When it’s cooking, add the <span class="item-8">tomatoes</span>, <span class="item-10">feta</span>, <span class="item-11">pepper</span> and <span class="item-12">salt</span>.</p>
 			<p>Let it simmer for quite a while</p>
 			<p>Cook the <span class="item-13">pasta</span>, and serve with <span class="item-14">grated cheese</span>, if you like. Alternatively you can use the sauce to create <a href="../puree-di-papa/">Puree di Papa</a></p>
 		</div>
